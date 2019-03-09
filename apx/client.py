41146c6af19750bb67408314d7097c2040e30723
sander-visser <github@visser.se>
2019-03-09 17:57:49 +0100

    Pylint client

--- apx/client.py
+++ apx/client.py
@@ -2,6 +2,7 @@ import apx
 import remotefile
 import abc
 
+
 class DataListener(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def on_data(self, port_id, port_name, data):
@@ -9,27 +10,28 @@ class DataListener(metaclass=abc.ABCMeta):
         called by apx.client when a require port has updated its data
         """
 
+
 @apx.NodeDataClient.register
 class Client:
     """
     APX Client class for a single APX node
     """
     def __init__(self, node=None):
-        self.fileManager=apx.FileManager()
-        self.socketAdapter=None
+        self.fileManager = apx.FileManager()
+        self.socketAdapter = None
         if isinstance(node, str):
             self.create_node(node)
         else:
             self.attach_node(node)
-        self.dataListener=None
+        self.dataListener = None
 
     def attach_node(self, node):
         """
         Attaches existing APX node to the client
         """
-        self.node=node if isinstance(node, apx.Node) else None
+        self.node = node if isinstance(node, apx.Node) else None
         if self.node is not None:
-            self.nodeData=apx.NodeData(node)
+            self.nodeData = apx.NodeData(node)
             inPortDataFile = self.nodeData.inPortDataFile
             outPortDataFile = self.nodeData.outPortDataFile
             definitionDataFile = self.nodeData.definitionFile
@@ -39,7 +41,7 @@ class Client:
                 self.fileManager.attachLocalFile(outPortDataFile)
             if definitionDataFile is not None:
                 self.fileManager.attachLocalFile(definitionDataFile)
-            self.nodeData.nodeDataClient=self
+            self.nodeData.nodeDataClient = self
 
     def create_node(self, apxText):
         """
@@ -51,7 +53,7 @@ class Client:
             self.attach_node(node)
 
     def connect_tcp(self, address, port):
-        self.socketAdapter=remotefile.TcpSocketAdapter()
+        self.socketAdapter = remotefile.TcpSocketAdapter()
         self.socketAdapter.setReceiveHandler(self.fileManager)
         if self.socketAdapter.connect(address, port):
             self.fileManager.start()
@@ -59,13 +61,13 @@ class Client:
             return True
         else:
             return False
-    
+
     def connectTcp(self, address, port):
         """
         Backwards-compatible version of connect_tcp
         """
         return self.connect_tcp(address, port)
-    
+
     def find(self, name):
         """
         Finds port in attached node
@@ -73,14 +75,14 @@ class Client:
         if self.node is not None:
             return self.node.find(name)
         return None
-        
+
     def stop(self):
         if self.socketAdapter is not None:
             self.socketAdapter.stop()
         self.fileManager.stop()
 
     def set_listener(self, dataListener):
-        self.dataListener=dataListener
+        self.dataListener = dataListener
 
     def on_require_port_data(self, port, value):
         if self.dataListener:
@@ -96,7 +98,7 @@ class Client:
                     test_port = self.node.providePorts[port.id]
                 if test_port is not port:
                     raise ValueError('Port {0.name} is not a require port of node {1.name}'.format(port, self.node))
-            if isinstance(identifier, str):
+            elif isinstance(identifier, str):
                 port = self.node.find(identifier)
             elif isinstance(identifier, int):
                 port = self.node.requirePorts[identifier]
@@ -104,14 +106,17 @@ class Client:
                     port = self.node.providePorts[identifier]
                 if port is None:
                     raise ValueError('Invalid port Id {0:d} not a require port of node {1.name}'.format(identifier, self.node))
-            return self.nodeData.read_require_port(port.id) if isinstance(port, apx.RequirePort) else self.nodeData.read_provide_port(port.id)
-    
+            if isinstance(port, apx.RequirePort):
+                return self.nodeData.read_require_port(port.id)
+            else:
+                return self.nodeData.read_provide_port(port.id)
+
     def read_port(self, identifier):
         """
         Backwards-compatible version of read
-        """        
+        """
         return self.read(identifier)
-    
+
     def write(self, identifier, value):
         if self.node is not None and self.nodeData is not None:
             if isinstance(identifier, apx.Port):
@@ -122,7 +127,7 @@ class Client:
                         raise ValueError('Cannot write to require port {0.name}.{1.name}'.format(self.node, port))
                     else:
                         raise ValueError('Port {0.name} is not a provide port of node {1.name}'.format(self.port, self.node))
-            if isinstance(identifier, str):
+            elif isinstance(identifier, str):
                 port = self.node.find(identifier)
                 if not isinstance(port, apx.ProvidePort):
                     raise ValueError('Port {0.name} is not a provide port of node {1.name}'.format(self.port, self.node))
@@ -131,15 +136,15 @@ class Client:
                 if port is None:
                     raise ValueError('Port {0.name} is not a provide port of node {1.name}'.format(self.port, self.node))
             self.nodeData.write_provide_port(port.id, value)
-    
+
     def write_port(self, identifier, value):
         """
         Backwards-compatible version of write
         """
         self.write(identifier, value)
-    
+
     def __enter__(self):
         return self
-    
+
     def __exit__(self, exc_type, exc_value, traceback):
         self.stop()
