f6100f62ac799bad14cd75348ab4d7aa0a6e4354
sander-visser <github@visser.se>
2019-03-09 17:48:51 +0100

    Pylint remotefile

--- remotefile/proto.py
+++ remotefile/proto.py
@@ -1,7 +1,6 @@
 
 def readLine(buf, iBegin, iEnd):
-   for i in range(iBegin,iEnd):
-      if(buf[i]==10):
-         return i
-   return iBegin
-      
+    for i in range(iBegin, iEnd):
+        if(buf[i] == 10):
+            return i
+    return iBegin
