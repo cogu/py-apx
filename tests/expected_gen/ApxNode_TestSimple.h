730c727955baa1f1a7712aeb9f62422c8647b20f
sander-visser <github@visser.se>
2019-04-05 08:02:06 +0200

    Add separate test case for simple generator without callback

---
+++ tests/expected_gen/ApxNode_TestSimple.h
@@ -0,0 +1,22 @@
+#ifndef APXNODE_TESTSIMPLE_H
+#define APXNODE_TESTSIMPLE_H
+
+#include "apx_nodeData.h"
+
+//////////////////////////////////////////////////////////////////////////////
+// CONSTANTS
+//////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////
+// FUNCTION PROTOTYPES
+//////////////////////////////////////////////////////////////////////////////
+void ApxNode_Init_TestSimple(void);
+apx_nodeData_t * ApxNode_GetNodeData_TestSimple(void);
+
+Std_ReturnType ApxNode_Read_TestSimple_RS32Port(sint32 *val);
+Std_ReturnType ApxNode_Read_TestSimple_RU8Port(uint8 *val);
+Std_ReturnType ApxNode_Write_TestSimple_PS8Port(sint8 val);
+Std_ReturnType ApxNode_Write_TestSimple_PU32Port(uint32 val);
+void TestSimple_inPortDataWritten(void *arg, apx_nodeData_t *nodeData, uint32_t offset, uint32_t len);
+
+#endif //APXNODE_TESTSIMPLE_H
