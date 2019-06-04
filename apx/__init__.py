a231e0f12578fe0f8459483a217c11b02a5c028f
cogu <congus8@gmail.com>
2019-06-04 14:35:38 +0200

    Changed version to 0.3.4

--- apx/__init__.py
+++ apx/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.3.3"
+__version__ = "0.3.4"
 from apx.base import *
 from apx.vm_base import *
 from apx.node import *
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def test_suite():
     return suite
 
 setup(name='apx',
-      version='0.3.3',
+      version='0.3.4',
       description='Official APX python toolchain and client',
       long_description=readme(),
       classifiers=[
