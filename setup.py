6c6e06dfe72cc7b777f5df69b95bd21c42a794ac
cogu <congus8@gmail.com>
2019-11-12 12:59:33 +0100

    Changed version to 0.4.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def test_suite():
     return suite
 
 setup(name='apx',
-      version='0.3.4',
+      version='0.4.0',
       description='Official APX python toolchain and client',
       long_description=readme(),
       classifiers=[
