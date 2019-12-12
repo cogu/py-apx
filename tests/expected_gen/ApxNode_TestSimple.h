f74740937621a96b675c46bf4616dbdbf84a0638
sander-visser <github@visser.se>
2019-12-12 22:12:27 +0100

    Refresh 0.4.0 generator tests and check array loop variable datatype

--- tests/expected_gen/ApxNode_TestSimple.h
+++ tests/expected_gen/ApxNode_TestSimple.h
@@ -15,8 +15,10 @@ apx_nodeData_t * ApxNode_Init_TestSimple(void);
 apx_nodeData_t * ApxNode_GetNodeData_TestSimple(void);
 bool ApxNode_IsConnected_TestSimple(void);
 
+Std_ReturnType ApxNode_Read_TestSimple_PS16ARPort256(sint16 *val);
 Std_ReturnType ApxNode_Read_TestSimple_RS32Port(sint32 *val);
 Std_ReturnType ApxNode_Read_TestSimple_RU8Port(uint8 *val);
+Std_ReturnType ApxNode_Write_TestSimple_PS16ARPort256(sint16 *val);
 Std_ReturnType ApxNode_Write_TestSimple_PS8Port(sint8 val);
 Std_ReturnType ApxNode_Write_TestSimple_PU32Port(uint32 val);
 void TestSimple_inPortDataWritten(void *arg, apx_nodeData_t *nodeData, uint32_t offset, uint32_t len);
